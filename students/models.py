ca18645be52a2773818d81f9a6d70ca494cd7a8b
Muhammad Rizo <115140091+Muhammadrizo04@users.noreply.github.com>
2023-11-29 14:54:29 +0500

    Update models.py

--- students/models.py
+++ students/models.py
@@ -1187,3 +1187,19 @@ class SoftWare(models.Model):
                                 ContentFile(pptx_buffer.read()), save=False)
 
         super(SoftWare, self).save(*args, **kwargs)
+
+
+class Other(models.Model):
+    ism = models.CharField(max_length=50)
+    familya = models.CharField(max_length=50)
+    sharif = models.CharField(max_length=50)
+    berilgan_vaqt = models.CharField(max_length=200)
+    create_date = models.DateTimeField(auto_now_add=True)
+    seria = models.CharField(max_length=3)
+    sertificate_id = models.CharField(max_length=7, unique=True, blank=True)
+    sertificate_id_numeric = models.IntegerField(unique=True, blank=True)
+    pptx_file = models.FileField(upload_to='pptx_other', blank=True)
+    sertificate_front = models.FileField(upload_to='other_ser_front/')
+
+    def __str__(self):
+        return self.ism
